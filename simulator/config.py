2283fa11539821a659189275d05e26f778d5e174
SignInAnupama <adham772@students.campbellsville.edu>
2026-05-15 15:04:07 -0400

    Update config.py

--- simulator/config.py
+++ simulator/config.py
@@ -2,3 +2,18 @@ endpoint-type iot:Data-ATS --region us-east-2
 {
     "endpointAddress": "a3lhxzikpqmbfa-ats.iot.us-east-2.amazonaws.com"
 }
+
+ 
+ENDPOINT  = "a3lhxzikpqmbfa-ats.iot.us-east-2.amazonaws.com"  # <-- replace this
+PORT      = 8883          # MQTT over TLS - always 8883 for AWS IoT Core
+# Cert paths - relative to simulator/ folder
+CERT_PATH = "certs/device-cert.pem.crt"
+KEY_PATH  = "certs/private.pem.key"
+CA_PATH   = "certs/AmazonRootCA1.pem"
+ 
+CLIENT_ID = "mes-simulator"
+TOPIC     = "mes/machines"   # messages publish to mes/machines/CNC-01 etc.
+ 
+# Alert thresholds
+DOWNTIME_ALERT_MINUTES = 5   # alert if machine down longer than this
+PUBLISH_INTERVAL_SEC   = 5   # send a reading every 5 seconds
